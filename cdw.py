ecced09d0ae67f59df145a36f6abfb2c5f0bb310
cdw <2805882689@qq.com>
2018-06-21 07:17:36 +0000

    message

--- cdw.py
+++ cdw.py
@@ -1,41 +1,59 @@
-#;laskjfdl;dsakjf;
-
 import matplotlib
 matplotlib.use('Agg')
+
 import torch
 from torch.autograd import Variable
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
 
-x=torch.linspace(-5,5,200)
-x=Variable(x)
-x_np=x.data.numpy()
+n_data=torch.ones(100,2)
+x0=torch.normal(2*n_data,1)
+y0=torch.zeros(100)
+x1=torch.normal(-2*n_data,1)
+y1=torch.ones(100)
+x=torch.cat((x0,x1),0).type(torch.FloatTensor)
+y=torch.cat((y0,y1),).type(torch.LongTensor)
 
-y_relu=F.relu(x).data.numpy()
-y_sigmoid=F.sigmoid(x).data.numpy()
-y_tanh=F.tanh(x).data.numpy()
-y_softplus=F.softplus(x).data.numpy()
 
-plt.figure(1, figsize=(8, 6))
-plt.subplot(221)
-plt.plot(x_np, y_relu, c='red', label='relu')
-plt.ylim((-1, 5))
-plt.legend(loc='best')
+x,y=Variable(x),Variable(y)
 
-plt.subplot(222)
-plt.plot(x_np, y_sigmoid, c='red', label='sigmoid')
-plt.ylim((-0.2, 1.2))
-plt.legend(loc='best')
+class Net(torch.nn.Module):   
+    def __init__(self,n_feature,n_hidden,n_output):
+        super(Net,self).__init__()
+        self.hidden=torch.nn.Linear(n_feature,n_hidden)
+        self.out=torch.nn.Linear(n_hidden,n_output)
 
-plt.subplot(223)
-plt.plot(x_np, y_tanh, c='red', label='tanh')
-plt.ylim((-1.2, 1.2))
-plt.legend(loc='best')
+    def forward(self,x):
+        x=F.relu(self.hidden(x))
+        x=self.out(x)
+        return x
 
-plt.subplot(224)
-plt.plot(x_np, y_softplus, c='red', label='softplus')
-plt.ylim((-0.2, 6))
-plt.legend(loc='best')
+net=Net(n_feature=2,n_hidden=10,n_output=2)
+print(net)
 
+optimizer=torch.optim.SGD(net.parameters(), lr=0.02)
+loss_func=torch.nn.CrossEntropyLoss()
+
+plt.ion()
+
+for t in range(100):
+    out=net(x)
+    loss=loss_func(out, y)
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    if t%2==0:
+        plt.cla()
+        prediction=torch.max(out,1)[1]
+        pred_y=prediction.data.numpy().squeeze()
+        target_y=y.data.numpy()
+        plt.scatter(x.data.numpy()[:,0],x.data.numpy()[:,1],c=pred_y,s=100,lw=0,cmap="RdYlGn")
+        accuracy=sum(pred_y==target_y)/200
+        plt.text(1.5,-4,"Accuracy=%.2f" % accuracy,fontdict={"size":20, "color":"red" })
+        plt.pause(0.1)
+
+plt.ioff()
 plt.show()
+
 print("finish")
