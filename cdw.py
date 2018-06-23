6511b1ae7b8bdec1f600541e9c4d47289ae9d40f
cdw <2805882689@qq.com>
2018-06-23 01:19:26 +0000

    message

--- cdw.py
+++ cdw.py
@@ -15,8 +15,9 @@ def save():
             torch.nn.ReLU(),
             torch.nn.Linear(10,1)
             )
+
     optimizer=torch.optim.SGD(net1.parameters(),lr=0.5)
-    loss_func=torch.nn.MsELoss()
+    loss_func=torch.nn.MSELoss()
 
     for t in range(100):
         prediction=net1(x)
@@ -25,6 +26,33 @@ def save():
         loss.backward()
         optimizer.step()
 
+    plt.figure(1,figsize(10,3))
+    plt.subplot(131)
+    plt.title('Net1')
+    plt.scatter(x.data.numpy(),y.data.numpy())
+    plt.plot(x.data.numpy(),prediction.data.numpy(),'r-',lw=5)
+
+    torch.save(net1,'net.pkl')
+    torch.save(net1.state_dict(),'net_params.pkl')
+
+def restore_net():
+    net2=torch.load('net.pkl')
+    prediction=net2(x)
+
+def restore_params():
+    net3=torch.nn.Sequential(
+            torch.nn.Linear(1,10),
+            torch.nn.ReLU(),
+            torch.nn.Linear(10,1)
+            )
+
+    net3.load_state_dict(torch.load('net_params.pkl'))
+    prediction=net3(x)
+
+
+save()
+restore_net()
+restore_params()
 
 
 print("finish")
