ae4a9530605d95654f23b7c1971008c94bfdf21d
cdw <2805882689@qq.com>
2018-06-23 02:40:21 +0000

    message

--- cdw.py
+++ cdw.py
@@ -4,55 +4,29 @@ matplotlib.use('Agg')
 import torch
 from torch.autograd import Variable
 import torch.nn.functional as F
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt 
+import torch.utils.data as Data
 
-x=torch.unsqueeze(torch.linspace(-1,1,100), dim=1)
-y=x.pow(2)+0.2*torch.rand(x.size())
+torch.manual_seed(1)
 
-def save():
-    net1=torch.nn.Sequential(
-            torch.nn.Linear(1,10),
-            torch.nn.ReLU(),
-            torch.nn.Linear(10,1)
-            )
+BATCH_SIZE=9
 
-    optimizer=torch.optim.SGD(net1.parameters(),lr=0.5)
-    loss_func=torch.nn.MSELoss()
+x=torch.linspace(1,10,10)
+y=torch.linspace(10,1,10)
 
-    for t in range(100):
-        prediction=net1(x)
-        loss=loss_func(prediction,y)
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+torch_dataset=Data.TensorDataset(x,y)
+loader=Data.DataLoader(
+        dataset=torch_dataset,
+        batch_size=BATCH_SIZE,
+        shuffle=True,
+        num_workers=2,
+        )
 
-    plt.figure(1,figsize(10,3))
-    plt.subplot(131)
-    plt.title('Net1')
-    plt.scatter(x.data.numpy(),y.data.numpy())
-    plt.plot(x.data.numpy(),prediction.data.numpy(),'r-',lw=5)
+for epoch in range(3):
+    for step,(batch_x,batch_y) in enumerate(loader):
+        print('Epoch',epoch, '| Step:',step, '| batch x:',
+                batch_x.numpy(), '| batch y:', batch_y.numpy() )
 
-    torch.save(net1,'net.pkl')
-    torch.save(net1.state_dict(),'net_params.pkl')
-
-def restore_net():
-    net2=torch.load('net.pkl')
-    prediction=net2(x)
-
-def restore_params():
-    net3=torch.nn.Sequential(
-            torch.nn.Linear(1,10),
-            torch.nn.ReLU(),
-            torch.nn.Linear(10,1)
-            )
-
-    net3.load_state_dict(torch.load('net_params.pkl'))
-    prediction=net3(x)
-
-
-save()
-restore_net()
-restore_params()
 
 
 print("finish")
