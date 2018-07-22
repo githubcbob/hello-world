907bc7c6bf50d65dc7ec14d5d566c07bdfeddbf9
cdw <2805882689@qq.com>
2018-07-22 11:45:06 +0800

    message

--- cdw.py
+++ cdw.py
@@ -1,72 +1,67 @@
 import matplotlib
 matplotlib.use('Agg')
-
+    
+import os 
 import torch
-from torch.autograd import Variable
-import torch.nn.functional as F
-import matplotlib.pyplot as plt 
+import torch.nn as nn
 import torch.utils.data as Data
+import torchvision
+import matplotlib.pyplot as plt
 
-LR=0.01
-BATCH_SIZE=32
-EPOCH=12
+EPOCH=1
+BATCH_SIZE=50
+LR=0.001
+DOWNLOAD_MNIST=False
 
-x=torch.unsqueeze(torch.linspace(-1,1,1000),dim=1)
-y=x.pow(2)+0.1*torch.normal(torch.zeros(*x.size()))
+if not(os.path.exists('./mnist/')) or not os.listdir('./mnist/'):
+        DOWNLOAD_MNIST=True
 
-plt.scatter(x.numpy(),y.numpy())
+train_data=torchvision.datasets.MNIST(
+        root='./mnist/',
+        train=True,
+        transform=torchvision.transforms.ToTensor(),
+
+        download=DOWNLOAD_MNIST,
+        )        
+
+print(train_data.train_data.size())
+print(train_data.train_labels.size())
+plt.imshow(train_data.train_data[0].numpy(), cmap='gray')
+plt.title('%i' % train_data.train_labels[0])
 plt.show()
 
-torch_dataset=Data.TensorDataset(x,y)
-loader=Data.DataLoader(dataset=torch_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=2,)
+train_loader=Data.DataLoader(dataset=train_data, batch_size=BATCH_SIZE, shuffle=True)
 
-class Net(torch.nn.Module):
+test_data=torchvision.datasets.MNIST(root='./mnist/', train=False)
+test_x=torch.unsqueeze(test_data.test_data, dim=1).type(torch.FloatTensor)[:2000]/255.
+test_y=test_data.test_labels[:2000]
+
+class CNN(nn.Module):
     def __init__(self):
-        super(Net,self).__init__()
-        self.hidden=torch.nn.Linear(1,20)
-        self.predict=torch.nn.Linear(20,1)
+        super(CNN,self).__init__()
+        self.conv1=nn.Sequential(
+                nn.Con2d(
+                    in_channels=1,
+                    out_channels=16,
+                    kernel_size=5,
+                    stride=1,
+                    padding=2,
+                    ),
+                nn.ReLU(),
+                nn.MaxPool2d(kernel_size=2),
 
-    def forward(self,x):
-       x=F.relu(self.hidden(x))
-       x=self.predict(x)
-       return x
+                )
 
-if __name__ == '__main__':
-    net_SGD =Net()
-    net_Momentum=Net()
-    net_RMSprop=Net()
-    net_Adam=Net()
-    nets=[net_SGD, net_Momentum, net_RMSprop, net_Adam]
+        self.conv2=nn.Sequential(
+                nn.Conv2d(16,32,5,1,2),
+                nn.ReLU(),
+                nn.MaxPoo12d(2),
 
-    opt_SGD=torch.optim.SGD(net_SGD.parameters(),lr=LR)
-    opt_Momentum=torch.optim.SGD(net_Momentum.parameters(), lr=LR,momentum=0.8)
-    opt_RMSprop=torch.optim.RMSprop(net_RMSprop.parameters(), lr=LR, alpha=0.9)
-    opt_Adam=torch.optim.Adam(net_Adam.parameters(), lr=LR, betas=(0.9, 0.99))
-    optimizers=[opt_SGD, opt_Momentum, opt_RMSprop, opt_Adam]
+                )
+        self.out=nn.Linear(32*7*7,10)
 
-    loss_func=torch.nn.MSELoss()
-    losses_his=[[],[],[],[]]
 
-    for epoch in range(EPOCH):
-        print('Epoch: ', epoch)
-        for step, (b_x, b_y) in enumerate(loader):
-            for net, opt, l_his in zip(nets, optimizers, losses_his):
-                output=net(b_x)
-                loss=loss_func(output, b_y)
-                opt.zero_grad()
-                loss.backward()
-                opt.step()
-                l_his.append(loss.data.numpy())
-     
-    labels=['SGD','Momentum','RMSprop','Adam']
-    for i, l_his in enumerate(losses_his):
-        plt.plot(l_his, label=labels[i])
-    plt.legend(loc='best')
-    plt.xlabel('Steps')
-    plt.ylabel('Loss')
-    plt.ylim((0, 0.2))
-    plt.show()
 
-    
-    
+
+
 print("finish")
