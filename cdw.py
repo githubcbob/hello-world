30dad72a3aeba0069167618b6220c0ee4e2845ea
cdw <2805882689@qq.com>
2018-07-22 15:19:33 +0800

    message

--- cdw.py
+++ cdw.py
@@ -60,7 +60,57 @@ class CNN(nn.Module):
                 )
         self.out=nn.Linear(32*7*7,10)
 
+     def forward(self,x):
+         x=self.conv1(x)
+         x=self.conv2(x)
+         x=x.view(x.size(0), -1)
+         ouput=self.out(x)
+         return output, x
 
+cnn=CNN()
+print(cnn)
+
+optimizer=torch.optim.Adam(cnn.parameters(), lr=LR)
+loss_func=nn.CrossEntropyLoss()
+
+form matplotlib import cm 
+try: from sklearn.manifold import TSNE; HAS_SK=True
+except:HAS_SK=False; print('Please install sklearn for layer visualization')
+def plot_with_labels(lowDWeights, labels):
+    plt.cla()
+    X,Y=lowDWeights[:,0],lowDWeight[:,1]
+    for x,y,s in zip(X, Y,labels):
+        c=cm.rainbow(int(255*s/9)); plt.text(x,y,s,backgroundcolor=c, fonsize=9)
+    plt.xlim(X.min(), X.max()); plt.ylim(Y.min(), Y.max()); plt.title('Visualize last layer'); 
+    plt.show(); plt.pause(0.01)
+
+plt.ion()
+for epoch in range(EPOCH):
+    for step, (b_x, b_y) in enumerate(train_loader):
+
+        output=cnn(b_x)[0]
+        loss=loss_func(output, b_y)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if step % 50 == 0:
+            test_output, last_layer = cnn(test_x)
+            pred_y = torch.max(test_output, 1)[1].data.squeeze().numpy()
+            accuracy=float((pred_y == test_y.data.numpy()).astype(int).sum()) / float(test_y.size(0))            print('Epoch: ', epoch, '| train loss: %.4f' % loss.data.numpy(), '| test accuracy: %.2f' % accuracy)
+            if HAS_SK:
+                tsne=TSNE(perplexity=30, n_components=2, init='pca', n_iter=5000)
+                plot_only=500
+                low_dim_embs=tsne.fit_transform(last_layer.data.numpy()[:plot_only, :])
+                labels=test_y.numpy()[:plot_only]
+                plot_with_labels(low_dim_embs, labels)
+plt.ioff()
+
+
+test_output, _=cnn(test_x[:10])
+pred_y =torch.max(test_output, 1)[1].data.numpy().squeeze()
+print(pred_y, 'prediction number')
+print(test_y[:10].numpy(), 'real number')
 
 
 
